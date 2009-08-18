cb57be005222738bc8ddce5cc85d677a60b576a4
danx0r <danbmil99@gmail.com>
2009-08-17 23:14:53 -0700

    integrity.py working on sirikata.com

--- integrity.py
+++ integrity.py
@@ -1,30 +1,38 @@
 #check integrity of CDN assets
 
-import os
-import hashlib
-from ftplib import FTP
+import os, sys
+##import hashlib
+##from ftplib import FTP
 
+"""
 def cb(s):
     fname = s.split()[-1]
     print fname
-
 
 ftp = FTP("sirikata.com")
 ftp.login("slartist", "V3sb4Dkb")
 ftp.dir("content/assets", cb)
 ftp.quit()
-
 """
+
+def sha256(filename):
+    cmd = "./sha256 -256 " + filename
+    sin, sout = os.popen2(cmd)
+    s = sout.read().split()[-1]
+    return s
+
 badfiles = []
 files = os.listdir(".")
 for i in files:
     if len(i)==64:
-        f=open(i)
-        s=f.read()
-        f.close()
-        sum = hashlib.sha256(s).hexdigest()
+        sum = sha256(i)
         if not i==sum:
-##            print "BAD file:", i, "sum:", sum
-            print "rm", i
+            print "remove bad file? (y/n)", i,
+            r=raw_input()
+            if r[0]=="y":
+                cmd = "rm " + i
+                print cmd
+                os.system(cmd)
             badfiles.append(i)
-"""
+        else:
+            print "good file:", i
