764a54eebf4432149dc9e3f3914a989f094c2cf1
danx0r <danbmil99@gmail.com>
2009-08-20 21:07:18 -0700

    integrity_names.py

--- integrity_names.py
+++ integrity_names.py
@@ -9,20 +9,29 @@ assets = os.listdir("../assets")
 
 for i in names:
     f = open(i)
-    s = f.read()
+    s = f.read().strip()
     f.close()
-    if len(s) != 73 and len(s) != 74:
+    if len(s) != 73:
         print i, "doesn't look like a name file", len(s)
+        badfiles.append("not a name file: " + i)
     else:
         s = s[-64:]
         if not s in assets:
+            badfiles.append("not in assets: " + i)
             print i, "points to", s, "which I can't find in ../assets"
             print "remove", i, "? (y/n)",
             r=raw_input()
             if r[0]=="y":
+                badfiles[-1] += "--removed"
                 cmd = "rm " + i
                 print cmd
                 os.system(cmd)
-            badfiles.append(i)
         else:
             print "good file:", i
+
+print "--------------------------------------------------------------"
+print "                          BAD FILES:"
+for i in badfiles:
+    print i
+print "--------------------------------------------------------------"
+
