f110a2eed96eab25f1ad038e98786b983aec74fd
danx0r <danbmil99@gmail.com>
2009-08-31 00:40:09 -0700

    added DDS check to integrity.py

--- integrity.py
+++ integrity.py
@@ -15,14 +15,44 @@ badfiles = []
 files = os.listdir(".")
 for i in files:
     if len(i)==64:
+##        print i
         sum = sha256(i)
         if not i==sum:
-            print "remove bad file? (y/n)", i,
-            r=raw_input()
-            if r[0]=="y":
-                cmd = "rm " + i
-                print cmd
-                os.system(cmd)
-            badfiles.append(i)
+            print "bad SHA file:", i
+            badfiles.append((i, "SHA"))
         else:
-            print "good file:", i
+            fil = i
+            try:
+                f = open(fil, "rb")
+            except:
+                continue
+            s = f.read(18)
+            f.close()
+            if s[:5] == "DDS |":
+                w = ord(s[12]) + ord(s[13])*256
+                h = ord(s[16]) + ord(s[17])*256
+                siz = os.path.getsize(fil)
+                res = w*h
+                est = int((siz-141)*1.5)
+                if not est >= res and (est-50) < res:
+                    print "BAD DDS file, resolution =", w, h, fil
+                    badfiles.append((i, "DDS"))
+                else:
+                    print "GOOD DDS file, resolution =", w, h, fil
+            else:
+                print "good file:", i
+
+if badfiles:
+    print "************************ BAD FILES: ****************************"
+    for i, err in badfiles:
+        print i, err
+    print "****************************************************************"
+    for i, err in badfiles:
+        print "remove bad file (y/n)", i, err
+        r=raw_input()
+        if r[0]=="y":
+            cmd = "rm " + i
+            print cmd
+            os.system(cmd)
+else:
+    print "No bad files found"
