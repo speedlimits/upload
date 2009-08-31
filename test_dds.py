0e0257d21d54b199128fc2cf63fe980a3887c674
danx0r <danbmil99@gmail.com>
2009-08-31 00:15:42 -0700

    DDS integrity check take 2

--- test_dds.py
+++ test_dds.py
@@ -7,20 +7,19 @@ else:
     files = os.listdir(".")
 
 for fil in files:
-    cmd = "file " + fil
-    sin, sout = os.popen2(cmd)
-    s = sout.read()
-    if "(DDS)" in s:
-        i = s.index("(DDS), ")+7
-        j = i+s[i:].index("x")
-        k = j+s[j:].index(",")
-        w = int(s[i:j-1])
-        h = int(s[j+1:k])
+    try:
+        f = open(fil, "rb")
+    except:
+##        print "Directory?"
+        continue
+    s = f.read(18)
+    f.close()
+    if s[:5] == "DDS |":
+        w = ord(s[12]) + ord(s[13])*256
+        h = ord(s[16]) + ord(s[17])*256
         siz = os.path.getsize(fil)
         res = w*h
         if not int((siz-141)*1.5) == res:
-            print fil, "is a BAD DDS file, resolution =", w, h
+            print "BAD DDS file, resolution =", w, h, fil
         else:
-            print fil, "is a GOOD DDS file, resolution =", w, h
-    else:
-        print fil, "is NOT a DDS file"
+            print "GOOD DDS file, resolution =", w, h, fil
